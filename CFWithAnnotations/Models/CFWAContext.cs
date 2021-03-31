58baf1ca6b74aa16de8598959d8fdd9af3b4b44f
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-03-31 16:44:01 +0530

    Add project files.

---
+++ CFWithAnnotations/Models/CFWAContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CFWithAnnotations.Models
+{
+    public class CFWAContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ CFWithAnnotations/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CFWithAnnotations.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DeptNo { get; set; }
+
+        [Required]
+        public string DeptName { get; set; }
+
+        public string Loc { get; set; }
+    }
+}
